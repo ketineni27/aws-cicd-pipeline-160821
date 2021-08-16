5b04d9fb6ea30e7404f629e260efa5fd81468cdd
ketineni27 <82327511+ketineni27@users.noreply.github.com>
2021-08-16 19:33:18 +0100

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-        bucket = "davoclock-aws-cicd-pipeline"
+        bucket = "cicds3bucket16082021"
         encrypt = true
         key = "terraform.tfstate"
         region = "us-east-1"
