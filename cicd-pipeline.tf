6925cb1889cedc7f69291355ebf432e9491ef726
ketineni27 <82327511+ketineni27@users.noreply.github.com>
2021-08-16 19:36:59 +0100

    Update cicd-pipeline.tf

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -69,7 +69,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             version = "1"
             output_artifacts = ["tf-code"]
             configuration = {
-                FullRepositoryId = "davoclock/aws-cicd-pipeline"
+                FullRepositoryId = "ketineni27/aws-cicd-pipeline-160821"
                 BranchName   = "master"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
